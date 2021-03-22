15b720c6100c1d2ba7fc7e377bbb6ac4a1c24574
carlosalberto <carlosalberto@positivosolucoes.com.br>
2021-03-22 19:06:00 -0300

    layout tela Category

--- Front/ComparaItens-App/src/app/category/category.component.ts
+++ Front/ComparaItens-App/src/app/category/category.component.ts
@@ -8,7 +8,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoryComponent implements OnInit {
 
-public categorys: any;
+  public categorys: any = [];
+  public categorysFiltred: any = [];
+
+  private _filterListCategory: string = '';
+
+  public get filterListCategory(): string {
+      return this._filterListCategory;
+  }
+
+  public set filterListCategory(value: string){
+    this._filterListCategory = value;
+    this.categorysFiltred = this.filterListCategory ? this.filterCategorys(this.filterListCategory): this.categorys;
+  }
+
+  filterCategorys(filterBy: string): any{
+    filterBy = filterBy.toLocaleLowerCase();
+    return this.categorys.filter(
+      (category: { description: string; }) => category.description.toLocaleLowerCase().indexOf(filterBy) !== -1
+    )
+  }
 
   constructor(private http: HttpClient) { }
 
@@ -20,7 +39,10 @@ public categorys: any;
 
     // tslint:disable-next-line: deprecation
     this.http.get('https://localhost:44324/v1/comparaItens/category/findAll').subscribe(
-      response => this.categorys = response,
+      response => {
+        this.categorys = response;
+        this.categorysFiltred = this.categorys;
+      },
       error => console.log(error)
     );
   }
