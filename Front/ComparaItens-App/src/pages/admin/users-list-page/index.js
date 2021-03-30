1cccc831c81f63965c1b875e6922bf2b88f8c8d8
Gustavo <gustavo.costa@altran.com.br>
2021-03-30 20:39:52 -0300

    fix undefined error

--- Front/ComparaItens-App/src/pages/admin/users-list-page/index.js
+++ Front/ComparaItens-App/src/pages/admin/users-list-page/index.js
@@ -1,59 +1,55 @@
-import React, {useEffect, useContext, useState} from 'react'
-import { Link } from "react-router-dom";
-import {AuthContext} from '../../../services/auth/auth-provider'
-import {findAll} from '../../../services/user/user-service'
+import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
+import { useAuthDataContext } from '../../../services/auth/auth-provider'
+import { findAll } from '../../../services/user/user-service'
 
 export default function AdminUserList() {
-    const {state, dispatch} = useContext(AuthContext)
-    const [users, setUsers] = useState([])
-    
+  const { token } = useAuthDataContext()
+  const [users, setUsers] = useState([])
 
-    useEffect(() => {
-        async function fetchUsers() {
-            console.log("token",state.token)
-            try {
-                const response = await findAll(state.token)
-                setUsers(response)
-                console.log('response',response)
-            } catch (error) {
-                console.error(error)
-            }
-        }
+  useEffect(() => {
+    async function fetchUsers() {
+      console.log('token', token)
+      try {
+        const response = await findAll(token)
+        setUsers(response)
+        console.log('response', response)
+      } catch (error) {
+        console.error(error)
+      }
+    }
 
-        fetchUsers()
-    }, [])
+    fetchUsers()
+  }, [])
 
-    
+  return (
+    <div className="user-list-container">
+      <Link to="/admin/user/add">
+        <button> add user</button>
+      </Link>
 
-    return (
-        <div className="user-list-container">
-            
-            <Link to="/admin/user/add" >
-                <button> add user</button>
-            </Link>
-
-            <table class="table">
-                <thead>
-                    <tr>
-                    <th scope="col">Nome</th>
-                    <th scope="col">Email</th>
-                    <th scope="col">Perfil</th>
-                    <th scope="col">Opções</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {users.map(user => {
-                        return (
-                            <tr>
-                                <th scope="row">{user.name}</th>
-                                <td>{user.email}</td>
-                                <td>{user.role}</td>
-                                <td></td>
-                            </tr>
-                        )
-                    })}
-                </tbody>
-            </table>
-        </div>
-    )
+      <table class="table">
+        <thead>
+          <tr>
+            <th scope="col">Nome</th>
+            <th scope="col">Email</th>
+            <th scope="col">Perfil</th>
+            <th scope="col">Opções</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user) => {
+            return (
+              <tr>
+                <th scope="row">{user.name}</th>
+                <td>{user.email}</td>
+                <td>{user.role}</td>
+                <td></td>
+              </tr>
+            )
+          })}
+        </tbody>
+      </table>
+    </div>
+  )
 }
