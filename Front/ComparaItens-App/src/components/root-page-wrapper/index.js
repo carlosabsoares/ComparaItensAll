be4627f6b2f77d4742be84fa8b504ec8b8e5c679
Gustavo <gustavo.costa@altran.com.br>
2021-04-02 20:00:16 -0300

    show side only to logged in user

--- Front/ComparaItens-App/src/components/root-page-wrapper/index.js
+++ Front/ComparaItens-App/src/components/root-page-wrapper/index.js
@@ -1,14 +1,17 @@
 import { Header } from 'components/admin/header/header'
 import { SideBar } from 'components/admin/sidebar'
+import { useAuthDataContext } from 'services/auth/auth-provider'
 
 import './styles.css'
 
 export function RootPageWrapper({ children }) {
-  return (
-    <div className="root-page-wrapper">
-      <SideBar />
+  const { token } = useAuthDataContext()
 
-      <div className="main-panel">
+  return (
+    <div className="main-panel">
+      {token ? <SideBar /> : null}
+
+      <div className={`main-panel ${token ? 'has-sidebar' : ''}`}>
         <Header />
         {children}
       </div>
