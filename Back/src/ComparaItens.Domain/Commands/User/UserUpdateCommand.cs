90455d0d957cb45e06d6cacb32a2c2e3c31ffd3b
carlosabsoares <carlosabsoares@gmail.com>
2021-04-28 00:05:46 -0300

    teste

--- Back/src/ComparaItens.Domain/Commands/User/UserUpdateCommand.cs
+++ Back/src/ComparaItens.Domain/Commands/User/UserUpdateCommand.cs
@@ -9,7 +9,7 @@ namespace ComparaItens.Domain.Commands
         public string Password { get; set; }
         public string Role { get; set; }
         public string Name { get; set; }
-        public string Email { get; set; }
+        public string Email { get; set; } 
 
         public void Validate()
         {
